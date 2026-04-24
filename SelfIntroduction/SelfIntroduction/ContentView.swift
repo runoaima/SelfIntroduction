13ab93e6015bc4d28316ffbfda8c2bef48ca8534
Haruto Fukumoto <runoaima98@gmail.com>
2026-04-24 15:56:47 +0900

    Create

---
+++ SelfIntroduction/SelfIntroduction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SelfIntroduction
+//
+//  Created by Haruto Fukumoto on 2026/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SelfIntroduction/SelfIntroduction/SelfIntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfIntroductionApp.swift
+//  SelfIntroduction
+//
+//  Created by Haruto Fukumoto on 2026/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SelfIntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
